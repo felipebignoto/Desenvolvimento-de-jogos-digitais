41de45b947d0d27b1f0f2cc3ec4e5c40c08985e0
fbignoto <felipe.bignoto@open-co.com>
2025-09-23 20:05:14 -0300

    aula 13 - 14

---
+++ aula13-14/tiro.js
@@ -0,0 +1,22 @@
+function Tiro(context, nave) {
+    this.context = context;
+    this.nave = nave;
+    //Posicionar o tiro no bico da nave
+    this.largura = 4;
+    this.altura = 20;
+    this.x = nave.x + nave.imagem.width / 2 - this.largura
+    this.y = nave.y - this.altura;
+    this.velocidade = 10;
+    this.cor = "red"
+}
+Tiro.prototype = {
+    atualizar: function() {
+        this.y -= this.velocidade;
+    },
+    desenhar: function() {
+        var ctx = this.context;
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+        ctx.restore();
+    }
+}
